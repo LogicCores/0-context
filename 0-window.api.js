f3c473855e48707adb78d225cc6609957de44652
Christoph Dorn <christoph@christophdorn.com>
2015-09-15 00:45:59 -0700

    initial

---
+++ 0-window.api.js
@@ -0,0 +1,18 @@
+
+/*
+
+TODO: Implement with ccjson runtime loader and factor into other cores.
+
+Requirements:
+
+  1. Global Singleton
+  2. Multiple instances
+  3. Inhert from instance
+
+Use-Case Example:
+
+  * Single context to represent all page entities within a single parent context (epoch)
+  * Multiple contexts to represent each page
+  * Initialize a schema of sub-contexts for each page ...
+
+*/
